dd68230eebd6e29c57a1bd3755d65bfab75787ae
Qing Li <12790403+QingLea@users.noreply.github.com>
2024-08-12 00:34:48 +0300

    Update start_potainer.sh

--- start_potainer.sh
+++ start_potainer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker volume create portainer_data
+
 docker run -d -p 9000:9000 --name=portainer --restart=unless-stopped \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data portainer/portainer-ce
