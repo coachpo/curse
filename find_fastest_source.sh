15bd6c7c144c589b98f4d46847c216f1889984c3
Qing Li <12790403+QingLea@users.noreply.github.com>
2024-08-12 01:48:05 +0300

    Update find_fastest_source.sh

--- find_fastest_source.sh
+++ find_fastest_source.sh
@@ -3,16 +3,26 @@
 # URL of the Launchpad mirror list
 MIRROR_LIST="https://launchpad.net/ubuntu/+archivemirrors"
 
-# Check if correct number of arguments are passed
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <architecture> <distribution> <repository>"
-  exit 1
+# Prompt for architecture if not provided as an argument
+if [ -z "$1" ]; then
+  read -p "Enter the architecture (e.g., amd64, i386, arm64, armhf, armel, powerpc): " ARCH
+else
+  ARCH=$1
 fi
 
-# Set to the architecture, distribution, and repository you're looking for
-ARCH=$1
-DIST=$2
-REPO=$3
+# Prompt for distribution if not provided as an argument
+if [ -z "$2" ]; then
+  read -p "Enter the Ubuntu distribution (e.g., precise, saucy, trusty, focal, jammy): " DIST
+else
+  DIST=$2
+fi
+
+# Prompt for repository if not provided as an argument
+if [ -z "$3" ]; then
+  read -p "Enter the repository (main, restricted, universe, multiverse): " REPO
+else
+  REPO=$3
+fi
 
 mirrorList=()
 # Retrieve the Launchpad mirror list and extract HTTP mirrors
@@ -34,4 +44,3 @@ done
 wait
 
 echo "All done!"
-
